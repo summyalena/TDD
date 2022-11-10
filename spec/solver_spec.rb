04ebf7714b64d9ac0f7dd957d6d8a8211af5f6b4
Lena <summyalena@gmail.com>
2022-11-10 22:29:14 +0100

    tdd task

---
+++ spec/solver_spec.rb
@@ -0,0 +1,18 @@
+require_relative './../solver.rb'
+
+describe 'Solver class' do
+    context 'this tests for solver class' do
+        it 'should test for factorial' do
+            solve = Solver.new
+            expect(solve.factorial(0)).to eq 1
+            expect(solve.factorial(5)).to eq 120
+            expect(solve.factorial(1)).to eql 1
+        end
+    end
+    context 'this tests for reverse' do
+        it 'should tests for reverse strings' do
+            reverse = Solver.new
+            expect(reverse.reverse())
+end
+
+  
